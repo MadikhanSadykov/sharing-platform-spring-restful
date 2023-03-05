215191b29639831f045eb0ab218f479a6e60c7a6
Madikhan Sadykov <madikhansadykov@gmail.com>
2023-03-05 13:57:02 +0600

    Initial commit

---
+++ src/main/java/com/madikhan/app/SharingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.madikhan.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharingPlatformApplication.class, args);
+	}
+
+}
